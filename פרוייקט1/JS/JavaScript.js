0b65b701ac71591f2eaba3a17bbee666823914ee
shani <shaninathan20@gmail.com>
2024-05-20 00:08:58 +0300

    boom pada boom first commit

---
+++ פרוייקט1/JS/JavaScript.js
@@ -0,0 +1,12 @@
+﻿var myDiv;
+
+function ShowBigImg(Obj) {
+    myDiv = document.querySelector('#divImg');
+    myDiv.style.display = "block";
+    var thesrc = Obj.src;
+    document.querySelector("#BigImg").src = thesrc;
+}
+
+function hideBigImg() {
+    myDiv.style.display = "none";
+}
